88549ba087b01165d72541fe5b656632a6c3c463
Ko-GyeongTae <kokt360@gmail.com>
2023-01-15 15:39:40 +0900

    Basic of Nestjs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, Redirect, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
-@Controller()
+@Controller({ host: 'localhost' })
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(@Req() req: Request): string {
+    console.log(req.hostname);
     return this.appService.getHello();
+  }
+
+  @Get('redirect/docs')
+  @Redirect('https://google.com', 302)
+  getDocs(@Query('version') version) {
+    if (version && version === '5') {
+      return { url: 'https://docs.nestjs.com/v5/' };
+    }
   }
 }
