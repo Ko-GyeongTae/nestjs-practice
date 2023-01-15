3a35dd9e4b77226f888fba8076e3cfeb8da258aa
Ko-GyeongTae <kokt360@gmail.com>
2023-01-15 15:46:30 +0900

    recover

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,13 +2,12 @@ import { Controller, Get, Query, Redirect, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Request } from 'express';
 
-@Controller({ host: 'localhost' })
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(@Req() req: Request): string {
-    console.log(req.hostname);
+  getHello(): string {
     return this.appService.getHello();
   }
 
