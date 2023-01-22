128fc0bafe6219bb6f502150e736ab8fbea60314
Ko-GyeongTae <kokt360@gmail.com>
2023-01-22 17:57:48 +0900

    테스트 제거

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Get, Query, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get()
   getHello(): string {
@@ -16,5 +20,10 @@ export class AppController {
     if (version && version === '5') {
       return { url: 'https://docs.nestjs.com/v5/' };
     }
+  }
+
+  @Get('/db-host-from-config')
+  getDatabaseHostFromConfigService(): string {
+    return this.configService.get('DATABASE_HOST');
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiController } from './api/api.controller';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      envFilePath:
+        process.env.NODE_ENV === 'production'
+          ? '.production.env'
+          : process.env.NODE_ENV === 'stage'
+          ? '.stage.env'
+          : '.development.env',
+    }),
+  ],
   controllers: [AppController, ApiController],
   providers: [AppService],
 })
